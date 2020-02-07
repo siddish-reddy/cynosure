e808abad69421c2fae008c020d0dae03f9ea3192
Likhith Shankar <46518029+LikhithShankarPrithvi@users.noreply.github.com>
2020-02-07 21:37:14 +0530

    Update config_paytm.php

--- Paytm/lib/config_paytm.php
+++ Paytm/lib/config_paytm.php
@@ -1,4 +1,4 @@
 <?php
 //Change the value of PAYTM_MERCHANT_KEY constant with details received from Paytm.
-define('PAYTM_MERCHANT_KEY', 'XXXXXXXXXXXXXXX'); 
+define('PAYTM_MERCHANT_KEY', 'kQ9_lQ8IwjB2KVzh'); 
 ?>
